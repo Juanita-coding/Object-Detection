675b2d1e79665d4e9f711ccc32f06e683544ac1a
Juanita-coding <79908844+Juanita-coding@users.noreply.github.com>
2021-07-20 19:58:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
 img="";
+status="";
+objects=[];
 
 function preload()
 {
-    img=loadImage('dog_cat.jpg');
+    img=loadImage('office.jpg');
 }
 
 function setup()
 {
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+  if(error)
+  {
+     console.log(error);
+  }
+      console.log(results);
+      objects=results;
+
+ 
 }
 
 function draw()
 {
-    image(img,0,0,640,420);
-    fill('#FF00FF');
-    text("Dog",45,75);
-    noFill();
-    stroke('#FF00FF');
-    rect(30,60,450,350);
+  image(img,0,0,640,420);
+  if(status != "")
+  {
+     for(i=0 ; i < objects.length ; i++)
+     {
+         document.getElementById("status").innerHTML="status : object detected";
+         fill('#00008b');
+         percent=floor(objects[i].confidence*100);
+         text(objects[i].label+" "+ percent +"%", objects[i].x +15,objects[i].y +15);
+         noFill();
+         stroke('#00008b');
+         rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+     } 
+  }
 }
