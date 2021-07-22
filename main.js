f714093ad2983b456508010bd65b1f4522ef454a
Juanita-coding <79908844+Juanita-coding@users.noreply.github.com>
2021-07-22 19:45:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,11 @@ function preload()
 
 function setup()
 {
-    canvas=createCanvas(640,420);
+    canvas=createCanvas(380,380);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    video.size(380,380);
     objectDetector=ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML="Status : Detecting Objects";
 
@@ -20,7 +23,7 @@ function modelLoaded()
 {
     console.log("Model Loaded!");
     status=true;
-    objectDetector.detect(img,gotResult);
+    
 }
 
 function gotResult(error,results)
@@ -37,17 +40,22 @@ function gotResult(error,results)
 
 function draw()
 {
-  image(img,0,0,640,420);
+  image(video,0,0,380,380);
   if(status != "")
-  {
+  { r = random(255);
+    g = random(255);
+    b = random(255);
+    objectDetector.detect(video,gotResult);
      for(i=0 ; i < objects.length ; i++)
-     {
+
+     {    
          document.getElementById("status").innerHTML="status : object detected";
-         fill('#00008b');
+         document.getElementById("number_of_objects").innerHTML="Number of Objects detected are : "+ objects.length;
+         fill(r,g,b);
          percent=floor(objects[i].confidence*100);
          text(objects[i].label+" "+ percent +"%", objects[i].x +15,objects[i].y +15);
          noFill();
-         stroke('#00008b');
+         stroke(r,g,b);
          rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
      } 
   }
